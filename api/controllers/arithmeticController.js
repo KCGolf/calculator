016eb15c493f4eaea935ba230b8b182c665854d0
Ken Collins <kcgolf59@outlook.com>
2021-02-11 15:39:58 -0800

    fixing the add function as demo broke it

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,8 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    //'add':      function(a,b) { return +a + +b },  klc Modifying the add function
-    'add': function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },  //klc Modifying the add function
+    //'add': function(a,b) { return a + b },  //klc because this is for demo
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
